d51011100079f7e847bf11cb9257875a9e9a439e
earl-stephens <stephensearl@hotmail.com>
2022-02-27 22:48:23 -0700

    Refactor to use a runtime exception

--- src/application/Thermostat.java
+++ src/application/Thermostat.java
@@ -1,14 +1,17 @@
 package application;
 
 public class Thermostat {
-	public void setTemperature (double temperature) throws Exception {
+	public void setTemperature (double temperature) {
 		setMachineTemperature(temperature);
 		System.out.println("Setting temperature to " + temperature);
 	}
 	
-	public void setMachineTemperature(double temperature) throws Exception {
+	//by using RuntimeException, can get rid of the throws Exception
+	//in the method declaration
+	//used for unchecked exceptions
+	public void setMachineTemperature(double temperature) {
 		if(temperature < 0 || temperature > 35) {
-			throw new Exception("Temperature out of range");
+			throw new RuntimeException("Temperature out of range");
 		}
 	}
 }
